5629e8e81acb371bbee377bc6d6f7f64fd4ed2e7
Revanth_Neelam <105050862+revanth3005@users.noreply.github.com>
2026-07-20 19:54:03 +0530

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -9,6 +9,6 @@
 //
 // Paste the two values below, then restart the dev server.
 export const DB_CONFIG = {
-  url: "",
-  authToken: "",
+  url: "libsql://shuttlepro-revanth3005.aws-ap-south-1.turso.io",
+  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODQxMjQwMDcsImlkIjoiMDE5ZjY1ZDktODEwMS03ZmUzLWFkZjktNGVjYTcyMDM5YTg4Iiwia2lkIjoiX05QYk5BZ2FtaHhkR2QwWEdjZHFjZXlucXZrLXA4VVpCR0xDanhUakxKNCIsInJpZCI6IjM2OTZmNjE1LWU4NWItNDI5Zi04M2JjLWMyNjViOWFjZmEwYiJ9.B8WwoiOrxOD_QyeaQ3vMcoYFinghkwkOJ18gA24JWUz26gRZc2WWbxfCCzB6TWUrqhbzosd25bPHDRZGRYDwCQ",
 };
